9430c6ce6adb72cc9cb4745ab7c879019aba0828
Mateus Gueler Machado <mateus.gueler@gmail.com>
2025-07-06 12:27:58 -0300

    config: Jest settings

---
+++ jest.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('jest').Config} */
+const config = {
+  // Remove default transform
+  transform: {},
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  moduleFileExtensions: ['js', 'cjs', 'json'],
+}
+
+export default config
